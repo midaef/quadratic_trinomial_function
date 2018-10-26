203c4f349473333990e17e5cb468d2e35fb01f1e
Danil <danil.present@mail.ru>
2018-10-26 12:49:00 +0800

    create plot and search points or graphs

--- main.py
+++ main.py
@@ -21,27 +21,26 @@ def create_plot(a, b, c):
 	if D < 0:
 		x_list.append(xo)
 		y_list.append(yo)
-		for i in range(1, 6):
-			x_list.append(i)
-		for j in range(len(x_list)):
-			for k in x_list:
-				func = a * k**2 + b *k + c
-				y_list.append(func)
-		# plt.plot(x_list, y_list)
-		# plt.show()
+		for k in range(1, 6):
+			x_list.append(k)
+			func = a * k**2 + b * k + c
+			y_list.append(func)
+		plt.plot(x_list, y_list)
+		plt.show()
 		return x_list, y_list
 
 	elif D > 0:
-		for i in range(1, 6):
-			x_list.append(i)
-		for j in range(1, 6):
-			for k in x_list:
-				func = a * k**2 + b *k + c
-				y_list.append(func)
-		# plt.plot(x_list, y_list)
-		# plt.show()
-		return x_list, y_list
+		for k in range(1, 6):
+			x_list.append(k)
+			func = a * k**2 + b * k + c
+			y_list.append(func)
+		plt.plot(x_list, y_list)
+		plt.show()
 
+		return x_list, y_list
+	elif D == 0:
+		pass
+		
 
 def f(a, b, c):
 	# a * x**2 + b * x + c
@@ -71,7 +70,6 @@ def main():
 	b = int(input('Input b: '))
 	c = int(input('Input c: '))
 	f(a, b, c)
-	p(create_plot(a, b, c))
 	cls()
 	p('Your args:\na= ' + str(a) + '\nb= ' + str(b) + '\nc= ' + str(c))
 	p('Discriminant: ' + str(D))
@@ -81,7 +79,17 @@ def main():
 	p('NULL FUNC.')
 	p('x_1 null: ' + str(x_null_1))
 	p('x_2 null: ' + str(x_null_2))
-	
+	create_plot(a, b, c)
+	p('X points: ')
+	ch = 0
+	for i in x_list:
+		ch+=1
+		p(str(ch) + ' - ' + str(i))
+	p('Y points: ')
+	ch1 = 0
+	for j in y_list:
+		ch1+=1
+		p(str(ch1) + ' - ' + str(j))
 
 
 if __name__ == '__main__':
